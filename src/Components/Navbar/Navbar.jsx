f1ae22327b67a3b99a9913cd9f3717f43a316c21
prerit85jain <coderpreritjain5181@gmail.com>
2024-06-18 18:59:53 +0530

    NavBar part 1

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import './Navbar.css'
+import logo from './logo.ico'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className="Container_Navbar">
+        <div className="Burger_Logo_Navbar">
+            <div className="Burger">
+                <p></p>
+                <p></p>
+                <p></p>
+            </div>
+            <div className='Logo_Div_Navbar'>
+                <img src={logo} alt="" />
+                <p className='Logo_Title_Navbar'>Youtube</p>
+            </div>
+        </div>
+    </div>
   )
 }
 
